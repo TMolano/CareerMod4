aad48f176b6f88113ba0fccec83c0f412167f506
Trevor M <temolano@fullsail.edu>
2018-08-23 23:14:27 -0400

    Update content

---
+++ js/index.js
@@ -0,0 +1,3 @@
+document.getElementsByClassName("page-description")[0].innerHTML = "Lorem ipsum dolor sit amet, " +
+    "consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
+    "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
