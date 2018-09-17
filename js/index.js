8fde70a97fc668dacc2ba723e4efb600be0bda61
Trevor M <temolano@fullsail.edu>
2018-09-16 23:54:26 -0400

    HTML/CSS fixes.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,15 @@
 const about = document.getElementById("about-description");
 const owner = document.getElementById("owner");
 const contact = document.getElementById("contact-info");
+const methods = document.getElementById("methods");
+const pageDesc = document.getElementsByClassName("page-description");
+const wwg = document.getElementById("wwg-accordian");
+const htp = document.getElementById("htp-accordian");
+const resource = document.getElementById("resources-accordian");
+const submit = document.getElementById("submit");
+const feedback = document.getElementById("feedback");
 
-function aboutPage() {
+    function aboutPage() {
 
 
     about.innerHTML = "The purpose of the site is to allow anybody at any skill level to learn and explore the field of woodcrafting and DIY crafts. " +
@@ -13,25 +20,60 @@ function aboutPage() {
     owner.innerHTML = "Ever since taking woodshop in high school, Steve has taken a major interest in woodcrafting and DIY crafts. " +
         "He wants to spread his knowledge and enthusiasm to others and inspire them to simply try to build and craft their own ideas.";
 
-}
+    }
 
     function contactPage() {
 
+        contact.innerHTML = "If you have any questions or comments, please feel free to contact us at:";
 
-        contact.innerHTML = "If you have any questions or comments, please feel free to contact us at:\n" +
-            "Email: theworkshop@gmail.com\n" +
-            "\n" +
-            "Find us on social media:\n" +
-            "Twitter: @TheWorkshop\n" +
-            "Facebook: facebook.com/TheWorkshop\n" +
-            "Youtube: youtube.com/user/TheWorkshop\n";
-
+        methods.innerHTML =
+            "<p>" + "Email: theworkshop@gmail.com" + "</p>" +
+            "<p>" + "Twitter: " + "<a href='https://twitter.com/'>" + "@TheWorkshop" + "</a>" + "</p>" +
+            "<p>" + "Facebook: " + "<a href='https://facebook.com/'>" + "Facebook.com/TheWorkshop" + "</a>" + "</p>" +
+            "<p>" + "Youtube: " + "<a href='https://youtube.com/'>" + "Youtube.com/user/TheWorkshop" + "</a>" + "</p>";
     }
 
-if(contact) {
-    contactPage.call();
-}
+    function wwgDescription(){
+    pageDesc[0].innerHTML = "Whether you are a beginner or experienced craftsman, find guides that will help you learn and improve your woodworking skills.";
+    }
 
-if(about) {
-    aboutPage.call();
-}
+    function htpDescription(){
+    pageDesc[0].innerHTML = "Satisfy your DIY cravings no matter what skill level you are with these step by step projects.";
+    }
+
+    function resourcesDescription(){
+    pageDesc[0].innerHTML = "Find even more information on everything woodworking. Recommended external videos, articles, and more.";
+    }
+
+
+    if(contact) {
+        contactPage.call();
+
+        submit.addEventListener("click", function(){
+            feedback.innerHTML = "Message sent." + "</br>" + "We will reply in the next 24 hours.";
+
+            const form = document.getElementById("contact");
+            form.reset();
+
+            window.scrollTo(0,document.body.scrollHeight);
+
+        });
+    }
+
+    if(about) {
+        aboutPage.call();
+    }
+
+    if(wwg){
+        wwgDescription.call();
+    }
+
+    if(htp){
+        htpDescription.call();
+    }
+
+    if(resource){
+        resourcesDescription.call();
+    }
+
+
