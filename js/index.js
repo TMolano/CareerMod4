e788aa0d019cee9e32c0d06777db138d4dfc8aec
Trevor M <temolano@fullsail.edu>
2018-09-21 23:25:09 -0400

    Final changes. HTML, CSS, and JS code added or corrected.

--- js/index.js
+++ js/index.js
@@ -11,6 +11,9 @@ const submit = document.getElementById("submit");
 const feedback = document.getElementById("feedback");
 const insertImagesHome = document.getElementsByClassName("insert-images-home");
 const contentHome = document.getElementsByClassName("content-home");
+const insertImagesWwg = document.getElementsByClassName("insert-images-wwg");
+const insertImagesHtp = document.getElementsByClassName("insert-images-htp");
+
 
 
 //functions
@@ -50,6 +53,103 @@ function homePage() {
 
 }
 
+function WoodworkingPage(){
+    insertImagesWwg[0].innerHTML = "<img src=" + "../../images/wood/cedar.gif alt='photo of cedar wood'" + ">";
+
+    insertImagesWwg[1].innerHTML = "<img src=" + "../../images/wood/oaktexture_mobile.jpg alt='photo of oak wood'" +
+        " srcset='../../images/wood/oaktexture_mobile.jpg 640w, ../../images/wood/oaktexture.jpg 1200w' " +
+        " sizes='(min-width:320px) 100vw, (min-width:768px) 100vw'" + ">";
+
+    insertImagesWwg[2].innerHTML = "<img src=" + "../../images/wood/particleboard_mobile.jpg alt='photo of particleboard'" +
+        " srcset='../../images/wood/particleboard_mobile.jpg 640w, ../../images/wood/particleboard.jpg 1200w' " +
+        " sizes='(min-width:320px) 100vw, (min-width:768px) 100vw'" + ">";
+
+    insertImagesWwg[3].innerHTML = "<img src=" + "../../images/wood/plywood_mobile.jpg alt='photo of plywood'" +
+        " srcset='../../images/wood/plywood_mobile.jpg 640w, ../../images/wood/plywood.jpg 1200w' " +
+        " sizes='(min-width:320px) 100vw, (min-width:768px) 100vw'" + ">";
+
+    insertImagesWwg[4].innerHTML = "<img src=" + "../../images/content/dovetail_mobile.jpg alt='photo of a dovetail cut'" +
+        " srcset='../../images/content/dovetail_mobile.jpg 640w, ../../images/content/dovetail.jpg 1920w' " +
+        " sizes='(min-width:320px) 100vw, (min-width:768px) 100vw'" + ">";
+
+    insertImagesWwg[5].innerHTML = "<img src=" + "../../images/content/miter-joint-lead_mobile.jpg alt='photo of a miter cut'" +
+        " srcset='../../images/content/miter-joint-lead_mobile.jpg 640w, ../../images/content/miter-joint-lead.jpg 1200w' " +
+        " sizes='(min-width:320px) 100vw, (min-width:768px) 100vw'" + ">";
+
+    insertImagesWwg[6].innerHTML = "<img src=" + "../../images/content/ripversuscross.jpg alt='photo of a rip versus cross cut'" + ">";
+
+    insertImagesWwg[7].innerHTML = "<img src=" + "../../images/content/buttjoint_mobile.jpg alt='photo of a butt joint'" +
+        " srcset='../../images/content/buttjoint_mobile.jpg 640w, ../../images/content/buttjoint.jpg 1200w' " +
+        " sizes='(min-width:320px) 100vw, (min-width:768px) 100vw'" + ">";
+
+    insertImagesWwg[8].innerHTML = "<img src=" + "../../images/content/handtools2_mobile.jpg alt='photo of hand tools'" +
+        " srcset='../../images/content/handtools2_mobile.jpg 640w, ../../images/content/handtools2.jpg 1200w' " +
+        " sizes='(min-width:320px) 100vw, (min-width:768px) 100vw'" + ">";
+
+    insertImagesWwg[9].innerHTML = "<img src=" + "../../images/content/tools_mobile.jpg alt='photo of power tools'" +
+        " srcset='../../images/content/tools_mobile.jpg 640w, ../../images/content/tools.jpg 1200w' " +
+        " sizes='(min-width:320px) 100vw, (min-width:768px) 100vw'" + ">";
+
+    insertImagesWwg[10].innerHTML = "<img src=" + "../../images/content/DEWALT-DWS709-Slide-Compound-Miter-Saw_mobile.jpg alt='photo of a miter saw'" +
+        " srcset='../../images/content/DEWALT-DWS709-Slide-Compound-Miter-Saw_mobile.jpg 640w, ../../images/content/DEWALT-DWS709-Slide-Compound-Miter-Saw.jpg 1200w' " +
+        " sizes='(min-width:320px) 100vw, (min-width:768px) 100vw'" + ">";
+
+    insertImagesWwg[11].innerHTML = "<img src=" + "../../images/content/stationarysaw_mobile.jpg alt='photo of a stationary saw'" +
+        " srcset='../../images/content/stationarysaw_mobile.jpg 640w, ../../images/content/stationarysaw.jpg 1200w' " +
+        " sizes='(min-width:320px) 100vw, (min-width:768px) 100vw'" + ">";
+
+}
+
+function howToProjectsPage(){
+    insertImagesHtp[0].innerHTML = "<img src=" + "../../images/content/bench_mobile.jpg alt='photo of a wooden bench'" +
+        " srcset='../../images/content/bench_mobile.jpg 640w, ../../images/content/bench.jpg 1200w' " +
+        " sizes='(min-width:320px) 100vw, (min-width:768px) 100vw'" + ">";
+
+    insertImagesHtp[1].innerHTML = "<img src=" + "../../images/content/shoeorg_mobile.jpg alt='photo of a wooden shoe organizer'" +
+        " srcset='../../images/content/shoeorg_mobile.jpg 640w, ../../images/content/shoeorg.jpg 1200w' " +
+        " sizes='(min-width:320px) 100vw, (min-width:768px) 100vw'" + ">";
+
+    insertImagesHtp[2].innerHTML = "<img src=" + "../../images/content/storagecontainer_mobile.jpg alt='photo of a wooden magazine holder'" +
+        " srcset='../../images/content/storagecontainer_mobile.jpg 640w, ../../images/content/storagecontainer.jpg 1200w' " +
+        " sizes='(min-width:320px) 100vw, (min-width:768px) 100vw'" + ">";
+
+    insertImagesHtp[3].innerHTML = "<img src=" + "../../images/content/woodenchair_mobile.jpg alt='photo of a wooden chair'" +
+        " srcset='../../images/content/woodenchair_mobile.jpg 640w, ../../images/content/woodenchair.jpg 1200w' " +
+        " sizes='(min-width:320px) 100vw, (min-width:768px) 100vw'" + ">";
+
+    insertImagesHtp[4].innerHTML = "<img src=" + "../../images/content/farmhouse-dining-table_mobile.jpg alt='photo of dining table'" +
+        " srcset='../../images/content/farmhouse-dining-table_mobile.jpg 320w, ../../images/content/farmhouse-dining-table.jpg 640w' " +
+        " sizes='(min-width:320px) 100vw, (min-width:768px) 100vw'" + ">";
+
+    insertImagesHtp[5].innerHTML = "<img src=" + "../../images/content/reclaimed%20wood%20shelf%20diy03_mobile.jpg alt='photo of wooden shelf'" +
+        " srcset='../../images/content/reclaimed%20wood%20shelf%20diy03_mobile.jpg 640w, ../../images/content/reclaimed%20wood%20shelf%20diy03.jpg 1200w' " +
+        " sizes='(min-width:320px) 100vw, (min-width:768px) 100vw'" + ">";
+
+    insertImagesHtp[6].innerHTML = "<img src=" + "../../images/content/lawnchair_mobile.jpg alt='photo of wooden lawn chair'" +
+        " srcset='../../images/content/lawnchair_mobile.jpg 320w, ../../images/content/lawnchair.jpg 640w' " +
+        " sizes='(min-width:320px) 100vw, (min-width:768px) 100vw'" + ">";
+
+    insertImagesHtp[7].innerHTML = "<img src=" + "../../images/content/storagebox_mobile.jpg alt='photo of wooden storage box'" +
+        " srcset='../../images/content/storagebox_mobile.jpg 320w, ../../images/content/storagebox.jpg 640w' " +
+        " sizes='(min-width:320px) 100vw, (min-width:768px) 100vw'" + ">";
+
+    insertImagesHtp[8].innerHTML = "<img src=" + "../../images/content/arch-top_mobile.jpg alt='photo of wooden arch top gate'" +
+        " srcset='../../images/content/arch-top_mobile.jpg 640w, ../../images/content/arch-top.jpg 1200w' " +
+        " sizes='(min-width:320px) 100vw, (min-width:768px) 100vw'" + ">";
+
+    insertImagesHtp[9].innerHTML = "<img src=" + "../../images/content/woodshed_mobile.jpg alt='photo of wooden shed'" +
+        " srcset='../../images/content/woodshed_mobile.jpg 640w, ../../images/content/woodshed.jpg 1200w' " +
+        " sizes='(min-width:320px) 100vw, (min-width:768px) 100vw'" + ">";
+
+    insertImagesHtp[10].innerHTML = "<img src=" + "../../images/content/cardtable_mobile.jpg alt='photo of a card table'" +
+        " srcset='../../images/content/cardtable_mobile.jpg 640w, ../../images/content/cardtable.jpg 1200w' " +
+        " sizes='(min-width:320px) 100vw, (min-width:768px) 100vw'" + ">";
+
+    insertImagesHtp[11].innerHTML = "<img src=" + "../../images/content/customworkbench_mobile.jpg alt='photo of a workbench'" +
+        " srcset='../../images/content/customworkbench_mobile.jpg 640w, ../../images/content/customworkbench.jpg 1200w' " +
+        " sizes='(min-width:320px) 100vw, (min-width:768px) 100vw'" + ">";
+}
+
 function aboutPage() {
 
 
@@ -69,9 +169,9 @@ function contactPage() {
 
     methods.innerHTML =
         "<p>" + "Email: theworkshop@gmail.com" + "</p>" +
-        "<p>" + "Twitter: " + "<a href='https://twitter.com/'>" + "@TheWorkshop" + "</a>" + "</p>" +
-        "<p>" + "Facebook: " + "<a href='https://facebook.com/'>" + "Facebook.com/TheWorkshop" + "</a>" + "</p>" +
-        "<p>" + "Youtube: " + "<a href='https://youtube.com/'>" + "Youtube.com/user/TheWorkshop" + "</a>" + "</p>";
+        "<p>" + "Twitter: " + "<a href='https://twitter.com/' target=\"_blank\">" + "@TheWorkshop" + "</a>" + "</p>" +
+        "<p>" + "Facebook: " + "<a href='https://facebook.com/' target=\"_blank\">" + "Facebook.com/TheWorkshop" + "</a>" + "</p>" +
+        "<p>" + "Youtube: " + "<a href='https://youtube.com/' target=\"_blank\">" + "Youtube.com/user/TheWorkshop" + "</a>" + "</p>";
 }
 
 function wwgDescription(){
@@ -80,6 +180,7 @@ function wwgDescription(){
 
 function htpDescription(){
     pageDesc[0].innerHTML = "Satisfy your DIY cravings no matter what skill level you are with these step by step projects.";
+    howToProjectsPage.call();
 }
 
 function resourcesDescription(){
@@ -112,6 +213,7 @@ if(about) {
 
 if(wwg){
     wwgDescription.call();
+    WoodworkingPage.call();
 }
 
 if(htp){
